075eb4324b78b8d88721fdf242319affaf9ef130
elena7993 <elenalee7993@gmail.com>
2025-02-10 18:50:47 +0900

    반복문연습

---
+++ 2일차/반복문연습.py
@@ -0,0 +1,32 @@
+# for i in range(1, 10):
+#   print(i)
+
+# for num in range(5):
+#   print(num)
+
+# num = 1
+# while num <= 5:
+#   print(num)
+#   num += 1
+
+  # +1씩 증가, 5가 되면 종료
+
+# num = int(input("숫자를 입력하세요"))
+
+# for gugudan in range(2, 10):
+#   print(f"{num} x {gugudan} = {num * gugudan}")
+
+# for num_1 in range(2, 10):
+#   for num_2 in range(11, 20):
+#     print(f"{num_1} x {num_2} = {num_1 * num_2}")
+#   print("___________")
+
+# count = 10
+# while count > 0:
+#   print(count)
+#   count -= 1
+# print("Happy New Year🎉")
+
+shows = ["그것이 알고싶다", "지선씨네마인드", "명탐정 코난"]
+for show in shows:
+  print(show)
